6fe5d62dfec4a2d23deb5e1f87b2e449d1f67b39
Kevin Li <yutengli@berkeley.edu>
2020-05-21 21:57:53 +0800

    backup at lec66.

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,11 +5,12 @@ import React from "react";
  * React component, whereas a class-based component needs to be Caps and extending
  * React.Component to be considered.
  */
+
 const Header = (props) => {
     return (
-        <div>
-            <h1>{props.title}</h1>
-            <h2>{props.subtitle}</h2>
+        <div className = "header">
+            <h1 className = "header__title">{props.title}</h1>
+            <h2 className = "header__subtitle">{props.subtitle}</h2>
         </div>
     );
 };
--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,6 @@
 /**
  *  Webpack file. A bundler.
  *  @author Kevin Li
- * 1. Output path has to be ABSOLUTE path.
- * 2. Dev-server does not explicitly create a bundle.js file.
- * 3. Inside /modules/rules, we use "loader" for one loader, and "use"
- *    if there is multiple.
  */
 const path = require("path");
 const _publicDir = path.join(__dirname, "public");
@@ -19,7 +15,7 @@ module.exports = {
     module: {
         rules: [
             { loader: "babel-loader", test: /\.js$/, exclude: /node_modules/ },
-            { test: /\.css$/, use: ["style-loader", "css-loader"] }
+            { test: /\.scss$/, use: ["style-loader", "css-loader", "sass-loader"] }
         ]
     },
     devtool: "eval-cheap-module-source-map",
@@ -27,3 +23,11 @@ module.exports = {
         contentBase: _publicDir
     }
 };
+/*
+* TODO 1. Output path has to be ABSOLUTE path.
+*  2. Dev-server does not explicitly create a bundle.js file.
+*  3. Inside /modules/rules, we use "loader" for one loader, and "use"
+*     if there is multiple.
+*  4. css-loader interprets @import and url() like import/require() and
+*     will resolve them. style-loader injects css into the DOM.
+*/
